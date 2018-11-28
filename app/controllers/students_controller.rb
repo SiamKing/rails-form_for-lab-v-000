43831b26dff2104d8ddd0dd6606d63e1daf0b82e
SiamKing <thesiameseelectric@gmail.com>
2018-11-28 07:24:44 -0800

    Finished again

---
+++ app/controllers/students_controller.rb
@@ -0,0 +1,38 @@
+class StudentsController < ApplicationController
+
+    def show
+        find_student
+    end
+
+    def new
+        @student = Student.new
+    end
+
+    def create
+        @student = Student.new(student_params)
+        @student.save
+        redirect_to student_path(@student)
+    end
+
+    def edit
+        find_student
+    end
+
+    def update
+        find_student
+        @student.update(student_params)
+        @student.save
+        redirect_to student_path(@student)
+    end
+
+    private
+
+    def find_student
+        @student = Student.find_by_id(params[:id])
+    end
+
+    def student_params
+        params.require(:student).permit(:first_name, :last_name)
+    end
+
+end
